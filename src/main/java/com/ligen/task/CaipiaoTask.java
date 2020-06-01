62fa22c578e91961c198374f15d208f07c8946f9
ligen <ligen@tartan-sh.com>
2020-06-01 18:18:26 +0800

    update

--- src/main/java/com/ligen/task/CaipiaoTask.java
+++ src/main/java/com/ligen/task/CaipiaoTask.java
@@ -4,6 +4,8 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.ligen.util.HttpInstance;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +13,12 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by ligen on 2018/3/11.
@@ -38,7 +44,13 @@ public class CaipiaoTask {
     public void 排列五() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         try {
-            String str = HttpInstance.create().doGet(pl5Url, null, 10);
+            OkHttpClient client = createClient();
+            Request req = new Request.Builder()
+                    .url(pl5Url)
+                    .get()
+                    .build();
+            String str = client.newCall(req).execute().body().string();
+//            String str = HttpInstance.create().doGet(pl5Url, null, 10);
             savePl5Data(str);
             logger.info("pl5 updated");
         } catch (Exception e) {
@@ -51,7 +63,13 @@ public class CaipiaoTask {
     public void 印尼五分彩() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         try {
-            String str = HttpInstance.create().doGet(ynwfcUrl, null, 10);
+            OkHttpClient client = createClient();
+            Request req = new Request.Builder()
+                    .url(ynwfcUrl)
+                    .get()
+                    .build();
+            String str = client.newCall(req).execute().body().string();
+//            String str = HttpInstance.create().doGet(ynwfcUrl, null, 10);
             saveData(str, "ynwfc", sdf);
             logger.info("ynwfc updated");
         } catch (Exception e) {
@@ -63,8 +81,15 @@ public class CaipiaoTask {
     @Scheduled(cron="0 0/10 * * * ?")
     public void 印尼分分彩() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
         try {
-            String str = HttpInstance.create().doGet(ynffcUrl, null, 10);
+            OkHttpClient client = createClient();
+            Request req = new Request.Builder()
+                    .url(ynffcUrl)
+                    .get()
+                    .build();
+            String str = client.newCall(req).execute().body().string();
+//            String str = HttpInstance.create().doGet(ynffcUrl, null, 10);
             saveData(str, "ynffc", sdf);
             logger.info("ynffc updated");
         } catch (Exception e) {
@@ -77,7 +102,14 @@ public class CaipiaoTask {
     public void 重庆时时彩() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         try {
-            String str = HttpInstance.create().doGet(cqsscUrl, null, 10);
+            OkHttpClient client = createClient();
+            Request req = new Request.Builder()
+                    .url(cqsscUrl)
+                    .get()
+                    .build();
+            String str = client.newCall(req).execute().body().string();
+//            String str = HttpInstance.create().doGet(cqsscUrl, null, 10);
+            System.out.println(str);
             saveData(str, "cqssc", sdf);
             logger.info("cqssc updated");
         } catch (Exception e) {
@@ -90,7 +122,13 @@ public class CaipiaoTask {
     public void 幸运五分彩() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         try {
-            String str = HttpInstance.create().doGet(xywfcUrl, null, 10);
+            OkHttpClient client = createClient();
+            Request req = new Request.Builder()
+                    .url(xywfcUrl)
+                    .get()
+                    .build();
+            String str = client.newCall(req).execute().body().string();
+//            String str = HttpInstance.create().doGet(xywfcUrl, null, 10);
             saveData(str, "xywfc", sdf);
             logger.info("xywfc updated");
         } catch (Exception e) {
@@ -132,5 +170,31 @@ public class CaipiaoTask {
             data.put("result", result);
             mongoTemplate.save(data, "pl5");
         }
+    }
+
+    public OkHttpClient createClient() {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        //设置连接超时时间
+        builder.connectTimeout(1, TimeUnit.MINUTES);
+        builder.readTimeout(1, TimeUnit.MINUTES);
+        //设置代理,需要替换
+        if (ProxyTask.proxyList != null && ProxyTask.proxyList.size() > 0) {
+            Random r = new Random();
+            int i = r.nextInt(ProxyTask.proxyList.size());
+            String host = ProxyTask.proxyList.getJSONObject(i).getString("host");
+            int port = ProxyTask.proxyList.getJSONObject(i).getIntValue("port");
+            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(host, port));
+            builder.proxy(proxy);
+            logger.info("proxy client: {}:{}", host, port);
+        } else {
+            logger.info("unproxy client");
+        }
+
+        //cookie管理器
+//        CookieManager cookieManager = new CookieManager();
+        OkHttpClient client = builder
+//                .cookieJar(new JavaNetCookieJar(cookieManager))
+                .build();
+        return client;
     }
 }
---
+++ src/main/java/com/ligen/task/ProxyTask.java
@@ -0,0 +1,55 @@
+package com.ligen.task;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.ligen.util.HttpInstance;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+
+@Component
+public class ProxyTask {
+
+    Logger logger = LoggerFactory.getLogger(getClass());
+    static String freeUrl = "https://www.kuaidaili.com/free";
+
+    public static JSONArray proxyList = new JSONArray();
+
+    @PostConstruct
+    public void init() {
+        crawler();
+    }
+
+    @Scheduled(cron="0 0 10 * * ?")
+    public void crawler() {
+        try {
+            proxyList.clear();
+            String str = HttpInstance.create().doGet(freeUrl, null, 10);
+            Document doc = Jsoup.parse(str);
+            Element root = doc.getElementById("list");
+            Elements trs = root.getElementsByTag("tr");
+            for (Element tr : trs) {
+                Elements tds = tr.getElementsByTag("td");
+                if (tds.isEmpty()) {
+                    continue;
+                }
+                Element td0 = tds.get(0);
+                Element td1 = tds.get(1);
+                proxyList.add(new JSONObject().fluentPut("host", td0.text()).fluentPut("port", td1.text()));
+            }
+            logger.info("proxyList updated");
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.info("pl5 error");
+        }
+    }
+
+}
