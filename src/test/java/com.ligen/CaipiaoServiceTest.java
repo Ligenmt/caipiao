74aba594534aef19ccb18d57ec1cb3c7e1675ff6
ligen <ligen@tartan-sh.com>
2020-10-12 17:30:11 +0800

    update

--- src/test/java/com.ligen/CaipiaoServiceTest.java
+++ src/test/java/com.ligen/CaipiaoServiceTest.java
@@ -1,21 +1,21 @@
-package com.ligen;
-
-import com.ligen.service.CaipiaoService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@SpringBootTest
-@RunWith(SpringRunner.class)
-public class CaipiaoServiceTest {
-
-    @Autowired
-    CaipiaoService caipiaoService;
-
-    @Test
-    public void testCompose1() {
-        caipiaoService.compose01("20200930140", 10);
-    }
-}
+//package com.ligen;
+//
+//import com.ligen.service.CaipiaoService;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@SpringBootTest
+//@RunWith(SpringRunner.class)
+//public class CaipiaoServiceTest {
+//
+//    @Autowired
+//    CaipiaoService caipiaoService;
+//
+//    @Test
+//    public void testCompose1() {
+//        caipiaoService.compose01("20200930140", 10);
+//    }
+//}
